406322f23b274d82cd5222c632ece573a9bf39ee
ROSALYN ROWE <rrowe404@gmail.com>
2020-05-20 16:31:55 -0400

    validation for rows/columns inputs

--- src/UI/Input/InputComponent.ts
+++ src/UI/Input/InputComponent.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, OnDestroy, Output, ElementRef } from "@angular/core";
-import { FormControl, FormGroup, ValidatorFn } from "@angular/forms";
+import { FormControl, FormGroup, ValidatorFn, Validators } from "@angular/forms";
 import { InputFocusEventService } from '../InputFocus/InputFocusEventService';
 
 /** Barrier between app and third-party inputs */
@@ -22,7 +22,11 @@ export class InputComponent implements OnDestroy, OnInit {
     @Input() public name: string;
     @Input() public formGroup: FormGroup;
     @Input() public type: string = 'text';
-    @Input() public validators: ValidatorFn[];
+    @Input() public validators: ValidatorFn[] = [];
+
+    @Input() public min: number;
+    @Input() public max: number;
+    @Input() public required: boolean = false;
 
     @Input() public value: string;
 
@@ -46,6 +50,18 @@ export class InputComponent implements OnDestroy, OnInit {
             throw new Error("All InputComponents must have a name!");
         }
 
+        if (this.min) {
+            this.validators.push(Validators.min(this.min));
+        }
+
+        if (this.max) {
+            this.validators.push(Validators.max(this.max));
+        }
+
+        if (this.required) {
+            this.validators.push(Validators.required);
+        }
+
         this.formControl = new FormControl(this.value ? this.value : '', this.validators);
 
         if (this.formGroup) {
@@ -60,10 +76,24 @@ export class InputComponent implements OnDestroy, OnInit {
     }
 
     public getErrorMessages() {
-        return Object.keys(this.formControl.errors).map(key => this.formControl.errors[key]);
+        return Object.keys(this.formControl.errors).map(key => this.getErrorMessage(key));
     }
 
     public updateValue(event: any) {
         this.onChange.emit(event.target.value);
+    }
+
+    private getErrorMessage(key: string) {
+        switch(key) {
+            case "required":
+                return "Required";
+
+            case "min":
+            case "max":
+                return `(${this.min}-${this.max})`;
+
+            default:
+                return this.formControl.errors[key];
+        }
     }
 }
