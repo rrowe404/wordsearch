a74ec28fa157c276f7498d915e1a25917e880ccf
ROSALYN ROWE <rrowe404@gmail.com>
2020-05-25 22:05:22 -0400

    extract logic that will be common to all slicer tests out

--- src/Rules/WordSearchStateSlicer/HorizontalWordSearchStateSlicer.spec.ts
+++ src/Rules/WordSearchStateSlicer/HorizontalWordSearchStateSlicer.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { HorizontalWordSearchStateSlicer } from './HorizontalWordSearchStateSlicer';
 import { WordSearchStateSlicerModule } from './WordSearchStateSlicerModule';
 import { WordSearchState } from '../WordSearchState/WordSearchState';
+import { WordSearchStateSlicerTestHelpers } from './WordSearchStateSlicerTestHelpers';
 
 describe('HorizontalWordSearchStateSlicer', () => {
     let service: HorizontalWordSearchStateSlicer;
@@ -26,19 +27,7 @@ describe('HorizontalWordSearchStateSlicer', () => {
             ['c', 'd']
         ];
 
-        let state = new WordSearchState();
-        state.setOptions({
-            alphabetizeWordList: false,
-            height: matrix.length,
-            width: matrix[0].length,
-            showWordList: false,
-            title: '',
-            words: []
-        });
-        state.seedMatrix(matrix);
-
-        let lettersWithPositions = state.getLettersWithPositions();
-        let slice = service.createSlice(state, lettersWithPositions);
+        let slice = WordSearchStateSlicerTestHelpers.getSlice(service, matrix);
 
         let row0 = slice[0];
         expect(row0[0].letter).toBe('a');
---
+++ src/Rules/WordSearchStateSlicer/WordSearchStateSlicerTestHelpers.ts
@@ -0,0 +1,23 @@
+import { WordSearchStateSlicer } from './WordSearchStateSlicer';
+import { WordSearchState } from '../WordSearchState/WordSearchState';
+
+/** Common logic for testing WordSearchStateSlicers */
+export class WordSearchStateSlicerTestHelpers {
+    public static getSlice(slicer: WordSearchStateSlicer, matrix: string[][]) {
+        let state = new WordSearchState();
+        state.setOptions({
+            alphabetizeWordList: false,
+            height: matrix.length,
+            width: matrix[0].length,
+            showWordList: false,
+            title: '',
+            words: []
+        });
+        state.seedMatrix(matrix);
+
+        let lettersWithPositions = state.getLettersWithPositions();
+        let slice = slicer.createSlice(state, lettersWithPositions);
+
+        return slice;
+    }
+}
