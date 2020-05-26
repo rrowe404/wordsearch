f51b77f7027296b56658c24085c3e8b8d4ed83d4
ROSALYN ROWE <rrowe404@gmail.com>
2020-05-25 22:41:57 -0400

    add tests for rectangular matrices to all slicer specs

--- src/Rules/WordSearchStateSlicer/BottomLeftToTopRightDiagonalWordSearchStateSlicer.spec.ts
+++ src/Rules/WordSearchStateSlicer/BottomLeftToTopRightDiagonalWordSearchStateSlicer.spec.ts
@@ -38,4 +38,51 @@ describe('BottomLeftToTopRightDiagonalWordSearchStateSlicer', () => {
         let row2 = slice[2];
         expect(row2[0].letter).toBe('b');
     });
+
+    it('should handle non-square matrices that have more columns than rows', () => {
+        let matrix = [
+            ['a', 'b', 'c'],
+            ['d', 'e', 'f']
+        ];
+
+        let slice = WordSearchStateSlicerTestHelpers.getSlice(service, matrix);
+
+        let row0 = slice[0];
+        expect(row0[0].letter).toBe('d');
+
+        let row1 = slice[1];
+        expect(row1[0].letter).toBe('e');
+        expect(row1[1].letter).toBe('a');
+
+        let row2 = slice[2];
+        expect(row2[0].letter).toBe('f');
+        expect(row2[1].letter).toBe('b');
+
+        let row3 = slice[3];
+        expect(row3[0].letter).toBe('c');
+    });
+
+    it('should handle non-square matrices that have more rows than columns', () => {
+        let matrix = [
+            ['a', 'b'],
+            ['c', 'd'],
+            ['e', 'f']
+        ];
+
+        let slice = WordSearchStateSlicerTestHelpers.getSlice(service, matrix);
+
+        let row0 = slice[0];
+        expect(row0[0].letter).toBe('e');
+
+        let row1 = slice[1];
+        expect(row1[0].letter).toBe('f');
+        expect(row1[1].letter).toBe('c');
+
+        let row2 = slice[2];
+        expect(row2[0].letter).toBe('d');
+        expect(row2[1].letter).toBe('a');
+
+        let row3 = slice[3];
+        expect(row3[0].letter).toBe('b');
+    });
 });
--- src/Rules/WordSearchStateSlicer/VerticalWordSearchStateSlicer.spec.ts
+++ src/Rules/WordSearchStateSlicer/VerticalWordSearchStateSlicer.spec.ts
@@ -36,4 +36,45 @@ describe('VerticalWordSearchStateSlicer', () => {
         expect(row1[0].letter).toBe('b');
         expect (row1[1].letter).toBe('d');
     });
+
+    it('should handle non-square matrices that have more columns than rows', () => {
+        let matrix = [
+            ['a', 'b', 'c'],
+            ['d', 'e', 'f']
+        ];
+
+        let slice = WordSearchStateSlicerTestHelpers.getSlice(service, matrix);
+
+        let row0 = slice[0];
+        expect(row0[0].letter).toBe('a');
+        expect(row0[1].letter).toBe('d');
+
+        let row1 = slice[1];
+        expect(row1[0].letter).toBe('b');
+        expect(row1[1].letter).toBe('e');
+
+        let row2 = slice[2];
+        expect(row2[0].letter).toBe('c');
+        expect(row2[1].letter).toBe('f');
+    });
+
+    it('should handle non-square matrices that have more rows than columns', () => {
+        let matrix = [
+            ['a', 'b'],
+            ['c', 'd'],
+            ['e', 'f']
+        ];
+
+        let slice = WordSearchStateSlicerTestHelpers.getSlice(service, matrix);
+
+        let row0 = slice[0];
+        expect(row0[0].letter).toBe('a');
+        expect(row0[1].letter).toBe('c');
+        expect(row0[2].letter).toBe('e');
+
+        let row1 = slice[1];
+        expect(row1[0].letter).toBe('b');
+        expect(row1[1].letter).toBe('d');
+        expect(row1[2].letter).toBe('f');
+    });
 });
