fa9804c179db6ae8f7365c50934c50f98fae2859
ROSALYN ROWE <rrowe404@gmail.com>
2020-05-28 23:11:23 -0400

    further the goal using an abstract base class

--- src/Rules/WordSearchOutput/ConsoleWordSearchOutputStrategy.ts
+++ src/Rules/WordSearchOutput/ConsoleWordSearchOutputStrategy.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { WordSearchOutputModule } from './WordSearchOutputModule';
-import { WordSearchOutputStrategy } from './WordSearchOutputStrategy';
-import { WordSearchState } from '../WordSearchState/WordSearchState';
+import { WordSearchOutputStrategyBase } from './WordSearchOutputStrategyBase';
 
 /** Debug only. Should not be available in prod mode. */
 @Injectable({
     providedIn: WordSearchOutputModule
 })
-export class ConsoleWordSearchOutputStrategy implements WordSearchOutputStrategy {
-    output(currentState: WordSearchState) {
-        console.log(currentState.title);
-        currentState.matrix.forEach(row => console.log(row));
-        console.log(currentState.totalMessage);
-        currentState.wordList.forEach(word => console.log(word));
+export class ConsoleWordSearchOutputStrategy extends WordSearchOutputStrategyBase {
+    protected outputTitle() {
+        console.log(this.currentState.title);
+    }
+
+    protected outputPuzzle() {
+        this.currentState.matrix.forEach(row => console.log(row));
+    }
+
+    protected outputTotal() {
+        console.log(this.currentState.totalMessage);
+    }
+
+    protected outputWordList() {
+        this.currentState.wordList.forEach(word => console.log(word));
     }
 }
---
+++ src/Rules/WordSearchOutput/WordSearchOutputStrategyBase.ts
@@ -0,0 +1,21 @@
+import { WordSearchOutputStrategy } from './WordSearchOutputStrategy';
+import { WordSearchState } from '../WordSearchState/WordSearchState';
+
+export abstract class WordSearchOutputStrategyBase implements WordSearchOutputStrategy {
+    protected currentState: WordSearchState;
+    
+    /** Don't override this */
+    public output(currentState: WordSearchState) {
+        this.currentState = currentState;
+
+        this.outputTitle();
+        this.outputPuzzle();
+        this.outputWordList()
+        this.outputTotal()
+    }
+
+    protected abstract outputTitle();
+    protected abstract outputPuzzle();
+    protected abstract outputTotal();
+    protected abstract outputWordList();
+}
