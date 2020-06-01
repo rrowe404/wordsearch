4ef440ca2d40cd31d74b672921700e4daaf79e98
ROSALYN ROWE <rrowe404@gmail.com>
2020-05-31 20:24:46 -0400

    add another fieldset, adjust styles

--- src/UI/InputList/InputListComponent.ts
+++ src/UI/InputList/InputListComponent.ts
@@ -4,15 +4,6 @@ import { InputFocusEventService } from '../InputFocus/InputFocusEventService';
 
 @Component({
     selector: 'wordsearch-input-list',
-    styles: [`
-        :host {
-            position: relative;
-        }
-
-        wordsearch-icon-button {
-            position: absolute;
-        }
-    `],
     template: `
         <div *ngFor="let input of inputs; let i = index;">
             <wordsearch-input [name]="getName(i)" [formGroup]="formGroup" [validators]="validators"></wordsearch-input>
