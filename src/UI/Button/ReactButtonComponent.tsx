534044d445c61bd040d9933a1abda9bef74a8241
Rosalyn Rowe <rrowe404@gmail.com>
2020-12-04 09:18:39 -0500

    introduce React and create a dummy component to show that it works

---
+++ src/UI/Button/ReactButtonComponent.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class ButtonComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return ( <div>Hello World</div> );
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ButtonComponent } from 'src/UI/Button/ReactButtonComponent';
 
 @Component({
   selector: 'wordsearch-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wordsearch';
+
+  ngOnInit() {
+    let container = document.querySelector('#react_container');
+    ReactDOM.render(React.createElement(ButtonComponent), container);
+
+  }
 }
