0bccd002a07333413e5cbce3dca349f9efaeb002
Rosalyn Rowe <rrowe404@gmail.com>
2020-12-18 15:09:27 -0500

    delete the old CheckboxModule

--- src/UI/UIModule.ts
+++ src/UI/UIModule.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CheckboxModule } from './Checkbox/CheckboxModule';
 import { WordSearchGeneratorFormModule } from './WordSearchGeneratorForm/WordSearchGeneratorFormModule';
 import { PlayableWordSearchModule } from './PlayableWordSearch/PlayableWordSearchModule';
 
 @NgModule({
     imports: [
-        CheckboxModule,
         PlayableWordSearchModule,
         WordSearchGeneratorFormModule
     ],
     exports: [
-        CheckboxModule,
         PlayableWordSearchModule,
         WordSearchGeneratorFormModule
     ]
--- src/UI/WordSearchGeneratorForm/WordSearchGeneratorFormModule.ts
+++ src/UI/WordSearchGeneratorForm/WordSearchGeneratorFormModule.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { WordSearchGeneratorFormComponent } from './WordSearchGeneratorFormComponent';
 import { CommonModule } from '@angular/common';
-import { CheckboxModule } from '../Checkbox/CheckboxModule';
 import { WordSearchOutputModule } from 'src/UI/WordSearchOutput/WordSearchOutputModule';
 import { PlayableWordSearchModule } from '../PlayableWordSearch/PlayableWordSearchModule';
 
 @NgModule({
     imports: [
         CommonModule,
-        CheckboxModule,
         PlayableWordSearchModule,
         WordSearchOutputModule
     ],
