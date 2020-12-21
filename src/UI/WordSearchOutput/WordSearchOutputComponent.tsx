64f11529e2605875039bf9b54e2764f5aebf8a0b
Rosalyn Rowe <rrowe404@gmail.com>
2020-12-21 17:24:32 -0500

    get at least the image generating again -- gonna need more work for the playable one !

--- src/UI/WordSearchOutput/WordSearchOutputComponent.tsx
+++ src/UI/WordSearchOutput/WordSearchOutputComponent.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { ReduxState } from '../Redux/ReduxState';
+import { WordSearchOutputStrategyFactory } from './WordSearchOutputStrategyFactory';
 
 export class WordSearchOutputComponent extends React.Component {
+    private wordSearchOutputStrategyFactory = new WordSearchOutputStrategyFactory();
+
     constructor(public props) {
         super(props);
     }
@@ -11,6 +14,9 @@ export class WordSearchOutputComponent extends React.Component {
         if (!this.props.wordSearchState) {
             return null;
         }
+
+        let output = this.wordSearchOutputStrategyFactory.createOutputStrategy(this.props.wordSearchState.outputOption);
+        output.output(this.props.wordSearchState);
 
         return (
             <div key={this.props.wordSearchState}>
