d148b360f6d8a70bb0e949e6bfff93a57ee8eba3
Rosalyn Rowe <rrowe404@gmail.com>
2023-10-24 18:16:50 -0700

    Swap ButtonComponent over to being an FC

--- src/UI/Button/ReactButtonComponent.tsx
+++ src/UI/Button/ReactButtonComponent.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import { ButtonProps } from './ButtonProps';
 
-export class ButtonComponent extends React.Component {
-    private color: string;
+const ButtonComponent: React.FC<ButtonProps> = ({
+  buttonType,
+  color = 'primary',
+  disabled,
+  text,
+}) => {
+  return (
+    <button type={buttonType} className={color} disabled={disabled}>
+      {text}
+    </button>
+  );
+};
 
-    constructor(public props: ButtonProps) {
-        super(props);
-
-        this.color = props.color || 'primary';
-    }
-
-    render() {
-        return (<button type={this.props.buttonType} className={this.color} disabled={this.props.disabled}>{this.props.text}</button>);
-    }
-}
+export { ButtonComponent };
