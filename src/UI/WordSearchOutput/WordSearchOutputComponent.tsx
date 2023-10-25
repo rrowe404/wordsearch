254bc97840033a28c5f72a2fc36eb0f801ee9caa
Rosalyn Rowe <rrowe404@gmail.com>
2023-10-24 18:49:55 -0700

    refactor WordSearchOutputComponent to an FC

--- src/UI/WordSearchOutput/WordSearchOutputComponent.tsx
+++ src/UI/WordSearchOutput/WordSearchOutputComponent.tsx
@@ -1,38 +1,39 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { WordSearchState } from 'src/Rules/WordSearchState/WordSearchState';
 import { ReduxState } from '../Redux/ReduxState';
 import { WordSearchOutputStrategyFactory } from './WordSearchOutputStrategyFactory';
 import './WordSearchOutputStyles.less';
 
-export class WordSearchOutputComponent extends React.Component {
-    private wordSearchOutputStrategyFactory = new WordSearchOutputStrategyFactory();
-
-    constructor(public props) {
-        super(props);
-    }
-
-    render() {
-        if (!this.props.wordSearchState) {
-            return this.outputContainer(null);
-        }
-
-        let output = this.wordSearchOutputStrategyFactory.createOutputStrategy(this.props.wordSearchState.outputOption);
-        let jsx = output.output(this.props.wordSearchState);
-
-        return this.outputContainer(jsx);
-    }
-
-    private outputContainer(content: JSX.Element) {
-        return (
-            <div className='output' key={this.props.wordSearchState}>
-                {content}
-            </div>
-        );
-    }
+interface Props {
+  wordSearchState: WordSearchState;
 }
 
+const wordSearchOutputStrategyFactory = new WordSearchOutputStrategyFactory();
+
+const WordSearchOutputContainer: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  return <div className='output'>{children}</div>;
+};
+
+const WordSearchOutputComponent: React.FC<Props> = ({ wordSearchState }) => {
+  if (!wordSearchState) {
+    return <WordSearchOutputContainer />;
+  }
+
+  let output = wordSearchOutputStrategyFactory.createOutputStrategy(
+    wordSearchState.outputOption
+  );
+  let jsx = output.output(wordSearchState);
+
+  return <WordSearchOutputContainer>{jsx}</WordSearchOutputContainer>;
+};
+
 let mapStateToProps = (state: ReduxState) => ({
-    wordSearchState: state.wordSearchState
+  wordSearchState: state.wordSearchState,
 });
 
-export const WordSearchOutputConnected = connect(mapStateToProps)(WordSearchOutputComponent);
+export const WordSearchOutputConnected = connect(mapStateToProps)(
+  WordSearchOutputComponent
+);
